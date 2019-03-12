2d0b7d8b13d0ddc88205cf77f4f42168bba487e2
Ancuta Daniel <ancutadaniel@gmail.com>
2019-03-12 20:24:27 +0200

    solve login user pass value for user

--- src/App.js
+++ src/App.js
@@ -2,23 +2,40 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route} from 'react-router-dom';
 import MainMenu from './shared/mainMenu';
-import HomePage from './views/pages/Homepage';
-import Login from './views/pages/Login';
+import UserContext from './shared/user.context';
+import { Homepage, Login } from './views/pages';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
 class App extends  React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: {}
+    };
+
+    this.handleUserChange = this.handleUserChange.bind(this);
+  }
+
+  handleUserChange(user) {
+    console.warn(user);
+    this.setState({ user });
+  }
+
   render() {
     return (
       <BrowserRouter>
-      <div className="App">
-          <MainMenu site_name="Delivery Food App"/>
-          <h1>Delivery Food App</h1>
-         
-          <Route exact path="/" component={ HomePage } />
-          {/* <Route exact path="/order" component={ OrderList } />*/}
-           <Route exact path="/login" component={ Login } />          
-      </div>
+        <UserContext.Provider value={ {user: this.state.user, handleUserChange: this.handleUserChange} }>
+          <div className="App">
+              <MainMenu site_name="Delivery Food App"/>
+              <h1>Delivery Food App</h1>
+            
+              <Route exact path="/" component={ Homepage } />
+              {/* <Route exact path="/order" component={ OrderList } />*/}
+              <Route exact path="/login" component={ Login } />          
+          </div>
+        </UserContext.Provider>
     </BrowserRouter>
     );
   }
--- src/views/pages/Login.js
+++ src/views/pages/Login.js
@@ -2,40 +2,61 @@ import React, { Component } from 'react';
 // import './App.css';
 import FacebookLogin from 'react-facebook-login';
 import GoogleLogin from 'react-google-login';
+import UserContext from '../../shared/user.context';
+import { Redirect } from 'react-router-dom';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      redirect: false
+    }
+  }
+  responseFacebook = (response) => {
+    //console.log(response);
+    console.log(this.context);
+    this.context.handleUserChange(response);
+    this.setState({
+      redirect: true
+    });
+  }
+
+  responseGoogle = (response) => {
+    this.context.handleUserChange(response);
+  }
 
   render() {
-
-    const responseFacebook = (response) => {
-      console.log(response);
-    }
-
-    const responseGoogle = (response) => {
-      console.log(response);
-    }
-
+      if (this.state.redirect === true) {
+        return <Redirect to='/' />
+      }
       return (
-          <div className="App">
-              <h2>LOGIN WITH:</h2>
+        <UserContext.Consumer>
+          { ({ user, handleUserChange }) => (
+            <div className="App">
+                <h2>LOGIN WITH:</h2>
 
-              <FacebookLogin
-                  appId="1992350001069404" //APP ID NOT CREATED YET
-                  fields="name,email,picture"
-                  callback={responseFacebook}
-              />
-              <br />
-              <br />
-              <GoogleLogin
-                  clientId="903697692149-j4q2rsmltp40gik0lb4ciqk3m9d59e4k.apps.googleusercontent.com" //CLIENTID NOT CREATED YET
-                  buttonText="LOGIN WITH GOOGLE"
-                  onSuccess={responseGoogle}
-                  onFailure={responseGoogle}
-              />
+                <FacebookLogin
+                    appId="1992350001069404" //APP ID NOT CREATED YET
+                    fields="name,email,picture"
+                    callback={this.responseFacebook}
+                />
+                <br />
+                <br />
+                <GoogleLogin
+                    clientId="903697692149-j4q2rsmltp40gik0lb4ciqk3m9d59e4k.apps.googleusercontent.com" //CLIENTID NOT CREATED YET
+                    buttonText="LOGIN WITH GOOGLE"
+                    onSuccess={this.responseGoogle}
+                    onFailure={this.responseGoogle}
+                />
 
-          </div>
+            </div>
+          ) }
+          </UserContext.Consumer>
     );
   }
 }
+
+Login.contextType = UserContext; 
 
 export default Login;

c7b9a024fae56f353b6f59a182e8140619df7bad
Miha Stroiescu <mihaela@stroiescu.ro>
2019-03-14 20:29:48 +0200

    Add image folder, working order page

---
+++ src/images/index.js
@@ -0,0 +1,23 @@
+import appeteasers from './appeteasers.jpg';
+import fino_side from './fino-side.jpg';
+import peri_peri_chicken from './peri-peri-chicken.jpg';
+import sharing_platters from './sharing-platters.jpg';
+import dessert from './dessert.jpg';
+import sides from './sides.jpg';
+import cataplana_algarve from './cataplana-algarve.jpg';
+import salads from './salads.jpg';
+import burgers_pitas_wraps from './burgers-pitas-wraps.jpg';
+import logo from './menu_logo.png';
+
+
+export {
+    appeteasers,
+    fino_side,
+    peri_peri_chicken,
+    sharing_platters,
+    dessert,
+    sides,
+    cataplana_algarve,
+    salads,
+    burgers_pitas_wraps,
+    logo };

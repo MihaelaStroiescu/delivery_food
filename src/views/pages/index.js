ae4f0e6306494ae0c861b74226d3e5f80cdf4e66
Daniel Ancuta <ancutadaniel@gmail.com>
2019-03-18 04:22:08 +0200

    ordermnu page

--- src/views/pages/index.js
+++ src/views/pages/index.js
@@ -1,9 +1,11 @@
 import Homepage from './Homepage';
 import Order from './Order';
 import Login from './Login';
+import OrderMenu from './OrderMenu';
 
 export {
     Homepage,
     Order,
-    Login
+    Login,
+    OrderMenu
 }
